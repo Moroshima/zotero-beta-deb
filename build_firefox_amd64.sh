3841f0003568bf1b276c2aeaa932ac92b1e14127
Moroshima <moroshimaitsuki@gmail.com>
2023-09-25 13:05:56 +0800

    finish firefox build script

--- build_firefox_amd64.sh
+++ build_firefox_amd64.sh
@@ -7,15 +7,26 @@ GECKO_VERSION=${GECKO_RELEASE//esr/}
 ROOT_DIR=$(pwd)
 FIREFOX_SOURCE_DIR=$ROOT_DIR/firefox-$GECKO_VERSION
 
-# basic tools
-sudo apt-get install -y wget gcc g++ make python3 nodejs npm
+# basic tools (zotero build deps)
+#sudo apt-get install -y git nodejs npm \
+#  python3 python3-pip
+
+# basic tools for building firefox
+sudo apt-get install -y wget xz-utils
+
+# deps provided by firefox mach debian.py file
 # firefox debian common packages deps
 sudo apt-get install -y build-essential libpython3-dev m4 unzip uuid zip
 # firefox debian browser common packages deps
 sudo apt-get install -y libasound2-dev libcurl4-openssl-dev libdbus-1-dev libdbus-glib-1-dev \
   libdrm-dev libgtk-3-dev libpulse-dev libx11-xcb-dev libxt-dev xvfb
-# mach build deps
-sudo apt-get install -y rustc cargo cbindgen llvm clang nasm
+
+# deps necessary for mach build
+sudo apt-get install -y rustc cbindgen clang nodejs nasm
+
+# mach build python3 deps
+#sudo apt-get install -y python3-pip python3-setuptools python3-wheel
+
 # mozconfig deps (libpng does not support apng, so we have to use firefox's libpng)
 sudo apt-get install -y libnspr4-dev libvpx-dev libnss3-dev libevent-dev
 echo "========================================================================"
@@ -45,6 +56,7 @@ mkdir -p "$ROOT_DIR/.mozbuild"
 
 export MOZCONFIG=$ROOT_DIR/mozconfig
 export MOZBUILD_STATE_PATH=$ROOT_DIR/.mozbuild
+export MACH_BUILD_PYTHON_NATIVE_PACKAGE_SOURCE="system"
 
 echo "Start building Firefox $GECKO_RELEASE x86_64..."
 ./mach build
@@ -56,10 +68,10 @@ echo "========================================================================"
 unset MOZCONFIG MOZBUILD_STATE_PATH
 
 if [ ! -f "$FIREFOX_SOURCE_DIR/firefox-build-dir/dist/firefox-$GECKO_VERSION.en-US.linux-x86_64.tar.bz2" ]; then
-  cp -f "$FIREFOX_SOURCE_DIR/firefox-build-dir/dist/firefox-$GECKO_VERSION.en-US.linux-x86_64.tar.bz2" "$ROOT_DIR/"
-else
-  echo "firefox-$GECKO_VERSION.en-US.linux-x86_64.tar.bz2 not found, there is something wrong during building process"
+  echo "firefox-$GECKO_VERSION.en-US.linux-x86_64.tar.bz2 not found, there is something wrong during building or packaging process!"
   exit 1
+else
+  cp -f "$FIREFOX_SOURCE_DIR/firefox-build-dir/dist/firefox-$GECKO_VERSION.en-US.linux-x86_64.tar.bz2" "$ROOT_DIR/"
 fi
 
 echo "Success! Firefox $GECKO_RELEASE x86_64 building progress done."
